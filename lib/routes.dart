8982d553dd4e26dfd9edd76e7180fb065b001aa9
Thabiso Inama <thabiso.inama@gmail.com>
2020-09-14 03:26:23 +0200

    Additional additions

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:wulf_store/screens/complete_profile/complete_profile_screen.dart';
 import 'package:wulf_store/screens/forgot_password/forgot_password_screen.dart';
+import 'package:wulf_store/screens/home/home_screen.dart';
 import 'package:wulf_store/screens/login_success/login_success_screen.dart';
 import 'package:wulf_store/screens/otp/otp_screen.dart';
 import 'package:wulf_store/screens/sign_in/sign_in_screen.dart';
@@ -17,4 +18,5 @@ final Map<String, WidgetBuilder> routes = {
   SignUpScreen.routeName: (context) => SignUpScreen(),
   CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
   OtpScreen.routeName: (context) => OtpScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
 };
--- lib/screens/splash/components/splash_content.dart
+++ lib/screens/splash/components/splash_content.dart
@@ -17,7 +17,7 @@ class SplashContent extends StatelessWidget {
       children: <Widget>[
         Spacer(),
         Text(
-          "WULF STORE",
+          "Wulf Store",
           style: TextStyle(
             fontSize: getProportionateScreenWidth(36),
             color: kPrimaryColor,
